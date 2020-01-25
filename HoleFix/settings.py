cfc48102db489e534d33e7ec311c1fd148c18da6
CodeCreator <37556311+Jackall1651@users.noreply.github.com>
2020-01-25 10:53:25 +0000

    Update settings.py

--- HoleFix/settings.py
+++ HoleFix/settings.py
@@ -121,5 +121,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-
-DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
