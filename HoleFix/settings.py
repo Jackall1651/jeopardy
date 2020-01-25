b7eeb958b580354d4246d01107c554707bd0f7a6
CodeCreator <37556311+Jackall1651@users.noreply.github.com>
2020-01-25 10:29:56 +0000

    Update settings.py

--- HoleFix/settings.py
+++ HoleFix/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 
+import dj_database_url
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_ROOT = os.path.join(BASE_DIR, "static/")
@@ -120,5 +122,4 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-import dj_database_url
 DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
